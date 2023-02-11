cd0eacec0efc2c42fe1da9869e2e1791fe73d11f
Ben Tracey <bptracey@btinternet.com>
2023-02-11 11:18:00 +0000

    footer fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,10 @@
 const Footer = () => {
   return (
     <>
-      <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-900 bottom-0 w-full">
+      <footer
+        className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-900 bottom-0 w-full position-fixed"
+        style={{ bottom: "0", width: "100%", zIndex: "1" }}
+      >
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2022 <a>Chitter™</a>. All Rights Reserved.
         </span>
